219669862708e8c48bcaa79d790382add05d4971
Jake T <crazybilly@gmail.com>
2014-09-28 21:34:05 -0500

    created new function, is.blank(), to look for blank, NA, NULL and NAN values.

---
+++ R/is.blank.R
@@ -0,0 +1,12 @@
+#' is.blank
+#' 
+#' tests a vector, returning TRUE if values are NA, NULL, blank strings or NAN
+#' @param x a vector to test for blank values
+#' @return a vector of logical values, where TRUE indicates blank values
+#' @export
+#' 
+is.blank  <- function(x) {
+  
+  is.na(x) | !nzchar(x) | is.null(x) | is.nan(x)
+  
+}
---
+++ R/packages.R
@@ -0,0 +1,1 @@
+# Install function for packages packages<-function(x){ x<-as.character(match.call()[[2]]) if (!require(x,character.only=TRUE)){ install.packages(pkgs=x,repos="http://cran.r-project.org") require(x,character.only=TRUE) } }
