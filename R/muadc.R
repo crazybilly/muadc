1158303c10eb37048937d54f2f24a1794fe680ae
crazybilly <crazybilly@gmail.com>
2014-10-02 13:35:03 -0500

    gave up on trying to build functions to install packages. Just install them yourself on the terminal.

--- R/muadc.R
+++
@@ -1,3 +0,0 @@
-#library(xlsx)
-library(plyr)
-library(dplyr)
--- R/packages.R
+++
@@ -1,1 +0,0 @@
-# Install function for packages packages<-function(x){ x<-as.character(match.call()[[2]]) if (!require(x,character.only=TRUE)){ install.packages(pkgs=x,repos="http://cran.r-project.org") require(x,character.only=TRUE) } }
