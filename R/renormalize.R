6f2626c90c465cb9a09489bac257408eeea1ad01
Jake T <crazybilly@gmail.com>
2014-10-07 01:32:24 -0500

    Started renormalize(), but didn't actually get anywhere. This will need to be built out as a real functions (I assume the arguments are x,i and sep? Need to look into hadley's lazyeval package, see if you can put that to use).

---
+++ R/renormalize.R
@@ -0,0 +1,8 @@
+renormalize  <- function {
+  
+  # turn this into a bearable function
+#   df <- read.csv('that-annoying-list.csv')
+#   activities <- strsplit(df$activities, "~")
+#   normalizedlist <- data.frame(user = rep(df$id, sapply(activities, length)), item = unlist(activities))
+#   
+}
