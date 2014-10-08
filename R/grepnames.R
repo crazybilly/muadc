1474bf017a9b1647ce1064f02825cae25104c0ed
Jake T <crazybilly@gmail.com>
2014-10-07 23:55:07 -0500

    tweaked grepnames. set default string as .* so you can run grepnames(,foo) and see all columns. also added sortalpha argument so you can sort the results by column name instead of by index, if so desired.

--- R/grepnames.R
+++ R/grepnames.R
@@ -1,7 +1,7 @@
 #' Search for a string in column names
 #' 
 #' Performs a grep regex search on the column names of a data frame, returning the values and their column number.
-#' @param str a regex string to find. 
+#' @param str a regex string to find. Defaults to ".*"
 #' @param x the object to search. Expect a data frame
 #' @param ignorecase should the search ignore case. Default = T
 #' @export
@@ -9,15 +9,20 @@
 #' grepnames("lifetimeg",hallp)
 #' grepnames("event",hallp,ignorecase=F)
 
-grepnames  <- function(str,x,ignorecase=T) {
-  df  <- as.data.frame(
-    grep(  str
-           , names(x)
-           ,ignore.case = T,value = F)
-    , grep(  str
+grepnames  <- function(str=".*",x,ignorecase=T,sortalpha=F) {
+  df  <- data.frame(
+      colname= grep(  str
              , names(x)
              ,ignore.case = T,value = T)
+      , index = grep(  str
+            , names(x)
+            ,ignore.case = T,value = F)
   )
-  names(df)  <- c("index")
+  #names(df)  <- c("index")
+  
+  if(sortalpha) {
+    df  <- df[with(df,order(colname)),]
+  }
+  
   return(df)
 }
