15d9565613efde702312dbee73c19719b7d95814
Jake T <crazybilly@gmail.com>
2014-10-07 22:33:18 -0500

    Built out renormalize(), which takes a list of denormailzed data (eg. a list of activities seperated by commas) and renormalizes it into a single column.
    Using tidyr seemed like the right solution, but it didn't play well with not being able to know how many entries there woudl be for each id.
    
    Kudos to the folks at StackOverflow for the implemented solution.

--- R/renormalize.R
+++ R/renormalize.R
@@ -1,8 +1,20 @@
-renormalize  <- function {
+#' renormalize
+#' 
+#' renormalize a denormalized list into a Boyce-Codd style data frame suitable for relational databases.
+#' @param x a datafame to analyze
+#' @param idcol a string describing the column name holding ids or the numeric value of the column's index
+#' @param listcol a string describing the column name holding the values to be re-normalized or the numeric value of the column's index
+#' @param sep a string describing the seperator between values in listcol. Defaults to ",".
+#' @param listcolname the name of the new column of re-normalized values. Defaults to 'normalized'.
+#' @return returns a 2-column data frame. The first column is a vector of ids from the original data frame. The second column is the renormalized data.
+
+renormalize  <- function (x,idcol,listcol,sep=",",listcolname='normalized'){
   
-  # turn this into a bearable function
-#   df <- read.csv('that-annoying-list.csv')
-#   activities <- strsplit(df$activities, "~")
-#   normalizedlist <- data.frame(user = rep(df$id, sapply(activities, length)), item = unlist(activities))
-#   
+  collist  <- strsplit(x[,listcol], sep)
+  normalizedlist <- data.frame(id = rep(x[,idcol], sapply(collist, length)), item = unlist(collist))
+  
+  names(normalizedlist)  <- c(idcol,listcolname)
+  
+  return(normalizedlist)
+  
 }
