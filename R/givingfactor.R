965e31b4ea46c1025a7816a4496664be850bd1c0
crazybilly <crazybilly@gmail.com>
2014-10-09 17:01:20 -0500

    started givingfactor() to bin stuff by WealthEngine categories. Use cut to make this happen.

---
+++ R/givingfactor.R
@@ -0,0 +1,7 @@
+givingfactor <- function(x) {
+  
+  # build this function out using cut so that you can 
+  # cut a vector into the WealthEngine bins (are these still
+  # the industry standard)?
+
+}
