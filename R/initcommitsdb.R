0bf7de818ee6a59742b13cda6c3cab45f6a5c786
crazybilly <crazybilly@gmail.com>
2014-11-11 15:38:41 -0600

    added hallp to initcommitsdb()

--- R/initcommitsdb.R
+++ R/initcommitsdb.R
@@ -1,10 +1,10 @@
-require(dplyr)
-
 #' initcommitsdb
 #' 
 #' a convinence function to connect to the commits database on localhost. 
 #' 
-#' @return Assigns a database connection, plus connections to the commit, gifts, pledges and memos tables in the global environment.
+#' @return Assigns a database connection, plus connections to the hallp commits, gifts, pledges and memos tables in the global environment.
+#' @export
+require(dplyr)
 initcommitsdb  <- function() {
   
     #connect to the database
@@ -13,16 +13,19 @@ initcommitsdb  <- function() {
     # connect to the tables in the db
     committbl  <- commitsdb  %>% tbl("commit")
     giftstbl   <- commitsdb  %>% tbl("gifts")
-    pledgestbl   <- commitsdb  %>% tbl("pledges")
+    pledgestbl <- commitsdb  %>% tbl("pledges")
     memostbl   <- commitsdb  %>% tbl("memos")
+    hallp      <- commitsdb  %>% tbl("hallp")
     
     # assign the tables (and the database connection) 
     #  in the global environment
-    assign("commitsdb",commitsdb,envir=globalenv())
+    assign("commitsdb",commitsdb,env=globalenv())
+    
     assign("committbl", committbl, env=globalenv())
     assign("giftstbl",  giftstbl,  env=globalenv())
     assign("pledgestbl",pledgestbl,env=globalenv())
     assign("memostbl",  memostbl,  env=globalenv())
+    assign("hallptbl",  hallp,     env=globalenv())
     
   
 }
