49b811d28f94fbbf3811e687e2bd9aecf86e1b57
crazybilly <crazybilly@gmail.com>
2015-01-05 09:55:15 -0600

    add na and ... functions to write.clip to allow users to pass functions to write.table().

--- R/write.clip.R
+++ R/write.clip.R
@@ -1,11 +1,13 @@
 #' Write data to the clipboard
 #' 
-#' A convience wrapper for write.table that writes tab-seperated data to the clipboard. Does not work in *nix.
+#' A convience wrapper for write.table that writes tab-seperated data to the Windows clipboard. Does not work in *nix.
 #' @param x an object to write
-#' @param sepr a string to seperate columns, passed to write.table as sep=sepr
+#' @param sepr a string to seperate columns, passed to write.table as sep=sepr. Defaults to \t, ie. tab-seperated columns. 
+#' @param na the string to replace NA values. Defaults to "".
+#' @param ... other arguments to pass to write.table()
 #' @export
-write.clip <- function(x,sepr='\t') {
-  write.table(x,file = 'clipboard-128',row.names=F,sep=sepr)
+write.clip <- function(x,sep='\t', na= "", ...) {
+  write.table(x,file = 'clipboard-128',row.names=F,sep=sep, na=na, ...)
 }
 
 
