750e1d648661f1b0146445407a4e657371816d1a
crazybilly <crazybilly@gmail.com>
2015-05-07 09:20:08 -0500

    Add invert.vector(). Increase clipboard size on write.clip, add argument to further increase size and move row.names=F as an argument to the function.

--- R/invert.vector.R
+++ R/invert.vector.R
@@ -2,7 +2,7 @@
 #' 
 #' Inverts a linear vector x, setting min(x) = min(x) and vice versa. Values in between are assumed to be linear values along the scale between min(x) and max(x).
 #' @param x a numeric vector to be inverted
-#' @value a numeric vector with length = length(x)
+#' @return a numeric vector with length = length(x)
 #' @export
 
 invert.vector <- function(x) {
--- R/write.clip.R
+++ R/write.clip.R
@@ -2,12 +2,18 @@
 #' 
 #' A convience wrapper for write.table that writes tab-seperated data to the Windows clipboard. Does not work in *nix.
 #' @param x an object to write
-#' @param sepr a string to seperate columns, passed to write.table as sep=sepr. Defaults to \t, ie. tab-seperated columns. 
+#' @param size the file size to use for the clipboard. Defaults to 4096 which is reasonably large.
+#' @param sepr a string to seperate columns, passed to write.table as sep=sepr. Defaults to tab-seperated columns. 
 #' @param na the string to replace NA values. Defaults to "".
+#' @param row.names a boolean values determining whether  row names be written to the file. Defaults to FALSE.
 #' @param ... other arguments to pass to write.table()
 #' @export
-write.clip <- function(x,sep='\t', na= "", ...) {
-  write.table(x,file = 'clipboard-128',row.names=F,sep=sep, na=na, ...)
+write.clip <- function(x,size='4096', sep='\t', na= "", row.names=F, ...) {
+  
+  destination = paste('clipboard-',size)
+  
+  write.table(x, file = destination, row.names = row.names, sep = sep, na = na, ...)
+   
 }
 
 
