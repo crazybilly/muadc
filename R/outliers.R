8ad0c0ad7e74315e3a834fb17fc1fd15c5e8d28d
crazybilly <crazybilly@gmail.com>
2015-05-29 10:17:30 -0500

    Add na.rm argument to outliers()

--- R/outliers.R
+++ R/outliers.R
@@ -2,6 +2,7 @@
 #' 
 #' Given a numeric vector, return the upper and lower bounds for outliers. Assumes IQR * 1.5 as bounds.
 #' @param x a numeric vector to be analysed.
+#' @param na.rm logical; if true, NA values  are removed before IQR and quantiles are computed.
 #' @export
 #' @return a list with two values:
 #' \enumerate{
@@ -9,17 +10,17 @@
 #'  \item top - the upper bound
 #'  }
 #'  
-outliers <- function(x) {
+outliers <- function(x, na.rm = T) {
   #calculate the thresholds
-  iqr <- IQR(x)
-  thirdq <- quantile(x)[4]
-  firstq <- quantile(x)[2]
+  iqr <- IQR(x, na.rm = na.rm)
+  thirdq <- quantile(x, na.rm = na.rm)[4]
+  firstq <- quantile(x, na.rm = na.rm)[2]
   outlier.top <- as.numeric((iqr*1.5) + thirdq)
   outlier.bottom <- as.numeric(firstq - (iqr*1.5))
   
   #if the threshold is smaller/larger than the min/max, there are no outliers, return NA
-  if( outlier.bottom < quantile(x)[1]) { outlier.bottom <- NA }
-  if( outlier.top > quantile(x)[5]) { outlier.top <- NA }
+  if( outlier.bottom < quantile(x, na.rm = na.rm)[1]) { outlier.bottom <- NA }
+  if( outlier.top > quantile(x, na.rm = na.rm)[5]) { outlier.top <- NA }
   
   #return a list
   outlier <- list("bottom" = outlier.bottom,"top" = outlier.top)
