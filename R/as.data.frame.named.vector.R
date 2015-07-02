de57b282324e126be2a23431c42bf6abf87934c0
crazybilly <crazybilly@gmail.com>
2015-07-02 10:49:42 -0500

    Create as.data.frame.named.vector. Edit variables for FY change. Rebuild documentation.

---
+++ R/as.data.frame.named.vector.R
@@ -0,0 +1,39 @@
+#' Coerce to a Data Frame
+#' 
+#' @description converts a named vector to a 2 column data frame with the names in the first column and values in the second columns. Throws an error if no names are found. Row names are stripped out of the final data frame.
+#' 
+#' @param x a named vector
+#' @param valname a character string to be used as the new column name. Defaults to the name of x
+#' 
+#' @return a two column data frame with nrow = length(x)
+#' @export
+
+
+
+as.data.frame.named.vector <- function(x,valname=deparse(substitute(x))) {
+  
+  
+  names <- names(x)
+  if(is.null(names)) {
+    names  <- row.names(x)
+  }
+  
+  if( is.null(names) | length(x) == 0 ) {
+    stop("no names found in x")
+  }
+  
+  df  <- data.frame( 
+        names = names
+      , val   = x
+    )
+  
+  if ("package:dplyr" %in% search() ) {
+    df  <- dplyr::tbl_df(df)
+  }
+  
+  df  <- setNames(df, c('names',valname))
+  row.names(df)  <- NULL
+  
+  return(df)
+  
+}
--- R/variables.R
+++ R/variables.R
@@ -1,6 +1,6 @@
 currentFY  <- 2015
 #figure out how to calculate these
-fyStartDate  <- as.Date("2014-07-01")
-fyEndDate	 <- as.Date("2015-06-30")
+fyStartDate  <- as.Date("2015-07-01")
+fyEndDate	 <- as.Date("2016-06-30")
 tmuGoal  <- 85000000
-afGoal	<-  2400000
+afGoal	<-  2500000
