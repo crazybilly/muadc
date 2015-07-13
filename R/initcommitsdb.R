3fa831df50fe8cd13307a7e82e80c75e6db774ac
crazybilly <crazybilly@gmail.com>
2015-07-13 09:56:45 -0500

    add database name argument to initcommitsdb().

--- R/initcommitsdb.R
+++ R/initcommitsdb.R
@@ -1,14 +1,16 @@
 #' Initialize my db connection
 #' 
 #' a convinence function to connect to the commits database on localhost. 
+#' @param db a string of the name of the database name with which you should connect. Defaults to the 'commits' db.
+#' 
 #' @return Assigns a database connection, plus connections to the hallp commits, gifts, pledges and memos tables in the global environment.
 #' @importFrom dplyr src_mysql
 #' @importFrom dplyr tbl
 #' @export
-initcommitsdb  <- function() {
+initcommitsdb  <- function(db = 'commits') {
   
     #connect to the database
-    commitsdb  <- dplyr::src_mysql("commits")
+    commitsdb  <- dplyr::src_mysql(db)
     
     # connect to the tables in the db
     committbl  <- commitsdb  %>% dplyr::tbl("commit")
