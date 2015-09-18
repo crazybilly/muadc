23061f058e22ed08b61e5c5ec15831891d3b05b7
crazybilly <crazybilly@gmail.com>
2015-09-18 13:32:37 -0500

    Create fillna(), a convience wrapper around replace(x,is.na(x),0)

---
+++ R/fillna.R
@@ -0,0 +1,15 @@
+  #' FillNA
+#' 
+#' @description Replaces NA values in a vector with a value, 0 by default. This is really just a convinence wrapper around replace() to minimize typing.
+#'
+#' @param x a vector with NA values to be filled
+#' @param fill a value to replace any NA values in x. Note that if the class of fill does not match the class of x, x will be coerced to an acceptable class.
+#'
+#' @return a vector with length of x
+#' @export
+#'
+fillna  <- function(x,fill = 0) {
+  
+  replace(x, is.na(x), fill)
+  
+}
