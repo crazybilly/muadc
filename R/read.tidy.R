e441dffc2113c3490b3c1bf98b5a19c519820a50
crazybilly <crazybilly@gmail.com>
2015-10-16 15:48:04 -0500

    Add default for read.tidy() to read from clipboard.

--- R/read.tidy.R
+++ R/read.tidy.R
@@ -2,14 +2,14 @@
 #'
 #' This is a convience wrapper for read.csv which lowercases column names and removes punctuation. It also has an option to only read the csv if the object does not exist. 
 #' 
-#' @param x the filename to load. Should be a csv
+#' @param x the filename to load. Should be a csv. Defaults to reading from the clipboard.
 #' @param ifexists should the function check if the object exists. Default = F
 #' @param existsname what object should the function look for, assuming ifexists = T. If ifexists == T and existname is not empty, the function will return the object with name = existsname.
 #' @param ... additional parameters to pass to read.csv
 #' @return Returns a data frame will lower cased names and all punctuation removed. If dplyr is already loaded, it returns a data frame as a tbl_df--otherwise, it returns a simple data frame.
 #' @export 
 
-read.tidy <- function(x,ifexists=F,existsname,...){
+read.tidy <- function(x='clipboard',ifexists=F,existsname,...){
   
   df  <- NULL
   
