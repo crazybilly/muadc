a180c81af7800a5a840fd4e22a58a468e86c6886
crazybilly <crazybilly@gmail.com>
2016-01-14 09:17:28 -0600

    Fix up typos in documentation. Make minor edit to read.tidy documentation to dodge error in R CMD CHK.

--- R/read.tidy.R
+++ R/read.tidy.R
@@ -3,7 +3,7 @@
 #' This is a convience wrapper for read.csv which lowercases column names and removes punctuation. It also has an option to only read the csv if the object does not exist. 
 #' 
 #' @param x the filename to load. Should be a csv. Defaults to reading from the clipboard.
-#' @param sep the field separator character. If x == 'clipboard', sep defaults '\t'--otherwise, it defaults to ',' to read csv data.
+#' @param sep the field separator character. If x == 'clipboard', sep defaults to tab seperated files. Otherwise, it defaults to ',' to read csv data.
 #' @param ifexists should the function check if the object exists. Default = F
 #' @param existsname what object should the function look for, assuming ifexists = T. If ifexists == T and existname is not empty, the function will return the object with name = existsname.
 #' @param ... additional parameters to pass to read.csv
