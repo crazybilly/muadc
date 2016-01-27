1a754e94c2401dfea84990fa2c5e3953caa6e0b5
crazybilly@gmail.com <crazybilly@gmail.com>
2016-01-27 08:20:05 -0600

    Revise eightytwenty() to consider each observation individually. Also, delete Read-and-delete-me.txt.

--- R/eightytwenty.R
+++ R/eightytwenty.R
@@ -5,59 +5,38 @@
 #' @param x a numeric vector to be analysed.
 #' @return a data frame with the following columns:
 #'  \enumerate{
-#'    \item x - the cutoff value
-#'    \item p.t - the percentage of total value (ie. the "80")
-#'    \item p.f - the percentage of observations above (ie. the "20")
+#'    \item cutoff - the cutoff value
+#'    \item perc_of_total_x - the percentage of total value (ie. the "80")
+#'    \item perc_of_rows  - the percentage of observations above the cutoff (ie. the "20")
 #'    }
 #' @export
 eightytwenty <- function(x) {
   
-  # grab the data from the input and create a freq table as dataframe
-  tab 	<- as.data.frame(table(x))
-  tab$x <- as.numeric(levels(tab$x))[tab$x]  
-  
-  #add the $ totals in 
-  tab$t <- tab$x * tab$Freq
-  
-  # sort the data by x asc
-  tab 	<- tab[order(tab$x),]
-  
-  #create cumulative totals
-  
-  # $ totals
-  tab$c.t <- cumsum(tab$t)
-  # frequency totals
-  tab$c.f <- cumsum(tab$Freq)
-  
-  # create percentage of totals
-  
-  # $ total percentages
-  tab$p.t <- tab$c.t/max(tab$c.t)
-  # frequency percentages
-  tab$p.f <- tab$c.f/max(tab$c.f)
-  
-  #sum percentages
-  tab$psum <- tab$p.f + tab$p.t
-  
-  #compare to 1
-  tab$dif <- 1-tab$psum
-  
-  #return the row closest to 1
-  results <- subset(tab,abs(dif)==min(abs(dif)),select=c(x,p.t,p.f,dif))
-  
-  
-  if ( results$dif != 0 ) {
-    remainderhalf <- results$dif/2
-    results$p.t <- results$p.t + remainderhalf
-    results$p.f <- results$p.f + remainderhalf
+    # set x in ascending order, and cumulative total
+    df <- data.frame( x = x[order(x)], cumtotal = cumsum(x))
+    
+    #set rownum
+    df$rownum <- 1:length(x)
+    
+    # calculate what % of the total is the cumulative total
+    df$perc_total <- df$cumtotal/sum(x, na.rm = T)
+    
+    # calculate what % of the number of rows is the current rows
+    df$perc_rows <- df$rownum/length(x)
+    
+    # add the percentages together
+    df$perc_plus_perc <- df$perc_total + df$perc_rows
+    
+    # compare the added percentages to 1 and take the absolute value
+    df$nearly1 <- abs(df$perc_plus_perc - 1)
+    
+    # grab the row where the added percentages are closest to zero
+    cutoff <- df[df$nearly1 == min(df$nearly1),]
+    
+    cutoff$perc_of_rows     <- round(1-cutoff$perc_rows,2)
+    cutoff$perc_of_total_x  <- round(1-cutoff$perc_total,2)
+    
+    cutoff$cutoff  <- cutoff$x
+    
+    cutoff[,c(10,9,8)]
   }
-  
-  
-  #round the resulting percentages off
-  results$p.t <- round(results$p.t,digits=2)
-  results$p.f <- round(results$p.f,digits=2)
-  
-  results <- subset(results,select = c(x,p.t,p.f))
-  
-  return(results)
-}
