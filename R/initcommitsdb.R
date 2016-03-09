603264294ebb3c5af9b271ac760ef888cf37ddb7
crazybilly@gmail.com <crazybilly@gmail.com>
2016-03-09 17:10:23 -0600

    Change default database to standing desk.

--- R/initcommitsdb.R
+++ R/initcommitsdb.R
@@ -2,6 +2,9 @@
 #' 
 #' a convinence function to connect to the commits database on localhost. 
 #' @param db a string of the name of the database name with which you should connect. Defaults to the 'commits' db.
+#' @param host the hostname or ip address for the database server
+#' @param user the database username 
+#' @param password the database password
 #' @param ... further arguments to pass to dplyr::src_mysql
 #' 
 #' @return Assigns a database connection, plus connections to the hallp commits, gifts, pledges and memos tables in the global environment.
@@ -9,10 +12,11 @@
 #' @importFrom magrittr %>% 
 #' @importFrom dplyr tbl
 #' @export
-initcommitsdb  <- function(db = 'commits', ...) {
+
+initcommitsdb  <- function(db = 'commits', host = '10.40.9.144', user = 'adc', password = 'goBigBlue', ...) {
  
     # connect to the database
-    commitsdb  <- dplyr::src_mysql(db, ...)
+    commitsdb  <- dplyr::src_mysql(db, host = host, user = user, password = password, ... )
     
     # connect to the tables in the db
     committbl  <- commitsdb  %>% dplyr::tbl("commit")
