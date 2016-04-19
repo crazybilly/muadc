a8edeaa05f40f65d03e8aeacf6721060976eab7a
crazybilly@gmail.com <crazybilly@gmail.com>
2016-04-19 11:13:25 -0500

    Add keep.underscore argument to clean.df(). Now removes all punctuation (except, by default, underscores).

--- R/clean.df.R
+++ R/clean.df.R
@@ -3,12 +3,21 @@
 #' Clean up a data frame by moving all column names to lower case, removing spaces and punctuation. Also, when dplyr is available, return the data frame as a tbl_df.
 #' 
 #' @param df a data frame 
+#' @param keep.underscore a logical value indicating whether underscores be kept or removed
+#' 
 #' @return a data frame, or if dplyr is available, a tbl_df. 
 #' @export
 
-clean.df <- function(df) {
+clean.df <- function(df, keep.underscore = TRUE) {
   
-  names(df) <- gsub("\\.|,|-| |_|\\/","",names(df))
+  if( keep.underscore) {
+    # remove everything except alphanumeric and underscore
+    names(df)  <- gsub("\\W", "", names(df) )
+  } else {
+    # remove punctuation and then remove the underscores, too
+    names(df)  <- gsub("_", "",  gsub("\\W", "", names(df) ) )
+  }
+    
   names(df) <- tolower(names(df))
   
   # if dplyr's already loaded, make the table a dplyr data_frame
