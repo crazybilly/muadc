19e3778ddfe1fabeca97271cf214706b66f8013b
crazybilly@gmail.com <crazybilly@gmail.com>
2016-05-10 10:28:02 -0500

    Add CIB campaigns vector. Also, trying currentFY variable as an .rda file in data/ to make variables work. If this works, move everything else from variables.R to data files.

--- R/variables.R
+++ R/variables.R
@@ -1,4 +1,4 @@
-assign('currentFY', 2015, envir = muadcEnv)
+assign('currentFY', 2016, envir = muadcEnv)
 #figure out how to calculate these
 assign('fyStartDate', as.Date("2015-07-01"), envir = muadcEnv)
 assign('fyEndDate	', as.Date("2016-06-30"), envir = muadcEnv)
