bfac1baf5af7a050c9ef664c3ad8726ecd1771dc
crazybilly@gmail.com <crazybilly@gmail.com>
2016-06-07 15:43:51 -0500

    Add ability in view.sample() to pass title to View.

--- R/view.sample.R
+++ R/view.sample.R
@@ -2,6 +2,7 @@
 #' 
 #' @description get a quick View of gigantic data, defaults to printing to RStudio's View().
 #' @param x an object to view. view.sample() is designed to work best with data frames but will work with vectors as well.
+#' @param title title for viewer window, pased to View().
 #' @param transpose a boolean value determining whether or not the data frame should be transposed. Defaults to TRUE so long data frames can be easily examined.
 #' @param sample.n an integer value determining how many rows should be sampled
 #' @param sample.method a string describing the way rows should be chosen from x. Valid values include:
@@ -12,7 +13,7 @@
 #' @return unless stdout==TRUE, does not return a value. Otherwise, returns the sampled data frame
 #' @export
  
-view.sample  <- function(x,transpose = TRUE,sample.n=3,sample.method='head',stdout=FALSE) {
+view.sample  <- function(x,title,transpose = TRUE,sample.n=3,sample.method='head',stdout=FALSE) {
   
   
   if (sample.method == 'head') {
@@ -35,7 +36,7 @@ view.sample  <- function(x,transpose = TRUE,sample.n=3,sample.method='head',stdo
   if (stdout == TRUE)  {
     z
   } else {
-    View(z)
+    View(z,title)
   }
   
 }
