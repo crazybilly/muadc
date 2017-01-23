f9f7674f31d9abe0bb7fc2cddef72657afac01e9
Jake Tolbert <jtolbert@at.millikin.edu>
2017-01-23 11:20:31 -0600

    Add excel=F argument to write.clip().

--- R/write.clip.R
+++ R/write.clip.R
@@ -5,14 +5,20 @@
 #' @param size the file size to use for the clipboard. Defaults to 4096 which is reasonably large.
 #' @param sepr a string to seperate columns, passed to write.table as sep=sepr. Defaults to tab-seperated columns. 
 #' @param na the string to replace NA values. Defaults to "".
-#' @param row.names a boolean values determining whether  row names be written to the file. Defaults to FALSE.
+#' @param row.names a logical value determining whether  row names be written to the file. Defaults to FALSE.
+#' @param excel a logical value determing whether Excel should be opened immediately after copying the data to the clipboard.
 #' @param ... other arguments to pass to write.table()
 #' @export
-write.clip <- function(x,size='4096', sep='\t', na= "", row.names=F, ...) {
+write.clip <- function(x,size='4096', sep='\t', na= "", row.names=F, excel = F, ...) {
   
   destination = paste('clipboard-',size)
   
   write.table(x, file = destination, row.names = row.names, sep = sep, na = na, ...)
+  
+  
+  if(excel) {
+    shell.exec('Excel')
+  }
    
 }
 
