ff19b29b40acb86e01a9d8c840e5ceca8d26c25e
Jake Tolbert <jtolbert@at.millikin.edu>
2017-01-31 13:58:18 -0600

    Add commitsandmemos().

--- R/giftsandmemos.R
+++ R/giftsandmemos.R
@@ -60,3 +60,68 @@ giftsandmemos  <-  function( collectdata = F) {
     
     
   }
+
+
+
+#' Create a List of Commitments and Memos
+#'  
+#'
+#' @param collectdata a logical value describing whether or not the data should be collected from the database
+#'
+#' @details If collectdata == T, returns a data frame of all commitments and memos with one line per transaction. If collectdata is FALSE, a tbl_sql with 6 columns:
+#' \itemize{
+#'  \item{"pidm"}{ constituent's id}
+#'  \item{"desg"}{ the designation number to which the gift was given}
+#'  \item{"campaign"}{ the campaign in which the gift was given}
+#'  \item{"fisc_code"}{ the fiscal year in which the gift was given}
+#'  \item{"amt"}{ the gift amount or the amount which was memo credited to the constituent}
+#'  \item{"dt"}{ the date the gift or memo was receipted}
+#' }
+#'     
+#' @return a 6 column data frame
+#'     
+#' @export
+#'
+commitsandmemos  <-  function( collectdata = F) {
+  
+  if( !exists("committbl" )) {
+    initcommitsdb()
+  }
+  
+  
+  commits  = committbl %>% 
+    select(
+      pidm 
+      , desg
+      , campaign
+      , fisc_code
+      , amt = commit_amt
+      , dt  = commit_date
+    )
+  
+  memos =  memostbl %>% 
+    select(
+      pidm = memo_pidm
+      , desg
+      , campaign
+      , fisc_code
+      , amt = memo_amt
+      , dt = gift_date
+    )
+  
+  if( collectdata ) {
+    bind_rows(
+        commit = commits %>% collect() 
+      , memo   = memos %>% collect() 
+      , .id    = 'type'
+    )
+  } else {
+    commits %<>% mutate(type = 'commit')
+    memos %<>% mutate(type = 'memo')
+    dplyr::union_all(commits,memos)
+  }
+  
+  
+  
+  
+}
