3e9d5b1444a82cdf8d89ed78b4f89ba234c2bcf4
Jake Tolbert <crazybilly@gmail.com>
2017-07-03 13:25:12 -0500

    Update variables for FY18 and add ability to turn off beeping in sourcebeep().

--- R/sourcebeep.R
+++ R/sourcebeep.R
@@ -5,13 +5,17 @@
 #' @param file a file to source
 #' @param echo should the call to source(file) be made with the echo argument set to TRUE
 #' @param ...  additional arguments to pass to `source()`
-#' @param beep a numerical value to pass to `beepr::beep()`, indicating what what sound should indicate the process is finished
+#' @param beep a numerical value to pass to `beepr::beep()`, indicating what what sound should indicate the process is finished. Set to NULL if 
 #'
 #' @export
 #'
 sourcebeep  <- function( file, echo = T, ..., beep = 1 ) {
   
   source(file, ..., echo=echo )
-  beepr::beep( beep)
+  
+  
+  if(!is.null(beep) ) {
+    beepr::beep( beep)
+  }
   
 }
