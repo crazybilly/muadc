72889a9bc06e15be4d1d85632a4927ec971bd7b3
Jake Tolbert <crazybilly@gmail.com>
2019-05-15 10:49:57 -0500

    Create appstdi_to_df() to convert appstdi.lis files into a data frame. Rework histrug to accept a data frame. This is still a little wonky and doesn't work quite right when passed a vector (instead of a data frame).

---
+++ R/appstdi-to-df.R
@@ -0,0 +1,39 @@
+#' APPSTDI to Data Frame
+#'
+#' @param appstdi a character vector read in from an appstdi.lis file (via `readr::read_lines()`) 
+#'
+#' @return a data frame with one line per person. Multiple degree lines are dropped.
+#' @export
+#' 
+#' @import stringr
+#' @import dplyr
+#'
+appstdi_to_df   <- function(appstdi) {
+  
+  namerowsi    <- stringr::str_detect(appstdi, "^00\\d\\d\\d\\d\\d\\d")
+  actionrowsi  <- stringr::str_detect(appstdi, "^(\\f)*                        .*20\\d\\d")
+  
+  namerows  <- appstdi[namerowsi] %>% 
+    stringr::str_split("  ", simplify = T) %>% 
+    dplyr::as_tibble(.name_repair = ~c("pidm", "name"))
+  
+  actionrows  <- appstdi_orig[actionrowsi]
+  
+  action  <- actionrows %>% 
+    stringr::str_replace("^\\s+(...).*", "\\1")
+  
+  dnrc  <- actionrows %>% 
+    stringr::str_replace("^\\s+(...)\\s+(....).*", "\\2")
+  
+  classyr  <- actionrows %>% 
+    stringr::str_replace("^\\s+(...)\\s+(....).*(20..).*", "\\3")
+  
+  
+  namerows$action  <- action
+  namerows$dnrc    <- dnrc
+  namerows$classyr <- classyr
+  
+  return(namerows)
+  
+  
+}
--- R/histrug.R
+++ R/histrug.R
@@ -1,10 +1,34 @@
 #' Plot a histogram with a rug
 #' 
 #' a convience wrapper to put a rug underneath a histogram. Also calls muplot for formatting tweaks
-#' @param x a numeric vector to plot
+#' 
+#' @param df either a data frame containing the column to be plotted, or a numeric vector to plot
+#' @param col if df is not null, then the unquoted name of a column in df to plot
+#' 
 #' @export
-histrug <- function(x) {
+histrug <- function(df, col) {
+  
   muplot()
-  hist(x)
-  rug(x)
+  
+  if(is.vector(df)) {
+    
+    hist(x)
+    rug(x)
+    
+  } else {
+    
+    colq  <- dplyr::enquo(col)
+    
+    thevector <-  df %>% 
+      dplyr::pull(!!colq)
+    
+    hist(thevector)
+    rug(thevector)
+      
+    
+    
+  }
+  
+  
+  
 }
